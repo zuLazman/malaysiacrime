cfdea84ed7f8db0f6371ae071df534610f897222
Kegan Gan <kegan@kebook.local>
2009-04-07 00:19:27 +0800

    Added Comments Framework urls.

--- urls.py
+++ urls.py
@@ -7,6 +7,7 @@ from django.conf.urls.defaults import *
 urlpatterns = patterns('',
     (r'^', include('main.urls')),
     (r'^crime/', include('crime.urls')),
+    (r'^comments/', include('django.contrib.comments.urls')),
 
     # Uncomment the admin/doc line below and add 'django.contrib.admindocs'
     # to INSTALLED_APPS to enable admin documentation:
