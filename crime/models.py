6e37d054a771361ac75abe4c3765bedce3dae738
Kegan Gan <kegan@kebook.local>
2009-04-10 17:25:21 +0800

    Fixed timestamp should be datetime.

--- crime/models.py
+++ crime/models.py
@@ -13,5 +13,5 @@ class Crime(models.Model):
     lng        = models.FloatField()
     details    = models.TextField()
     password   = models.CharField(max_length=20)
-    created_at = models.DateField(auto_now_add=True)
-    updated_at = models.DateField(auto_now=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
