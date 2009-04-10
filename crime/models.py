82daca1f9a4c8e4f4e230ebae7825d4bbfcb3dd5
Kegan Gan <kegan@kebook.local>
2009-04-10 17:25:40 +0800

    Added __unicode__.

--- crime/models.py
+++ crime/models.py
@@ -15,3 +15,6 @@ class Crime(models.Model):
     password   = models.CharField(max_length=20)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+
+    def __unicode__(self):
+        return self.headline
