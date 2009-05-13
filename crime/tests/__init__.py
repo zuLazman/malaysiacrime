b2f06c46acc618f222a3e41f81950e4f4395a8da
Kegan Gan <kegan@kebook.local>
2009-05-13 15:52:27 +0800

    Created tests for crime update.

--- crime/tests/__init__.py
+++ crime/tests/__init__.py
@@ -1,5 +1,6 @@
-from create_tests import *
 from show_tests import *
+from create_tests import *
+from update_tests import *
 
 # Disabled TEMPLATE_DIRS so that custom templates would not intefere with tests.
 from django.conf import settings
---
+++ crime/tests/update_tests.py
@@ -0,0 +1,66 @@
+from datetime import date
+from django.test import TestCase
+
+from crime.models import Crime
+
+
+class UpdateTestCase(TestCase):
+    """
+    Test updating a crime report.
+    """
+    urls = 'crime.urls'
+    fixtures = ['crimes']
+
+    def setUp(self):
+        pass
+
+    def test_get_update(self):
+        """
+        Test accessing update page.
+        """
+        response = self.client.get('/update/1/')
+        self.assertTemplateUsed(response, 'crime/update.html')
+
+        self.assertEquals(response.context['crime'].id, 1)
+        self.assertEquals(response.context['crime'].headline, "Terrible Crime")
+
+    def test_post_update(self):
+        """
+        Test post update page.
+        """
+        inputs = {
+            'headline': "Terrible Crime Updated",
+            'date': date(2009,12,31),
+            'location': "Ipoh, Perak",
+            'lat': 80,
+            'lng': 60,
+            'details': "Stealing of power.",
+            'author': "Nizar",
+            'password': "123456",
+        }
+        response = self.client.post('/update/1/', inputs)
+        self.assertRedirects(response, '/show/1/')
+
+        crime = Crime.objects.get(pk=1)
+        self.assertEquals(crime.headline, inputs['headline'])
+
+    def test_post_update_password_unmatch(self):
+        """
+        Test post update with unmatch password.
+        """
+        inputs = {
+            'headline': "Terrible Crime Updated",
+            'date': date(2009,12,31),
+            'location': "Ipoh, Perak",
+            'lat': 80,
+            'lng': 60,
+            'details': "Stealing of power.",
+            'author': "Nizar",
+            'password': "aaaaaa",
+        }
+        response = self.client.post('/update/1/', inputs)
+        self.assertTemplateUsed(response, 'crime/update.html')
+        self.assertFormError(response, 'form', 'password', "The password is incorrect.")
+
+    def tearDown(self):
+        pass
