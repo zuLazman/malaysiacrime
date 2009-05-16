f746ff259d6231bf38df98161b01ab3370bfc754
Kegan Gan <kegan@kebook.local>
2009-05-16 20:38:12 +0800

    Added tests for most updated crime reporting.

--- main/tests.py
+++ main/tests.py
@@ -1,7 +1,6 @@
 from django.test import TestCase
 
 
-
 class MainTestCase(TestCase):
     """
     Test accessing and browsing crime reports.
@@ -18,10 +17,22 @@ class MainTestCase(TestCase):
         response = self.client.get('/')
         self.assertTemplateUsed(response, 'main/index.html')
 
-        self.assertTrue(len(response.context['crimes']), 3)
-        self.assertTrue(response.context['crimes'][0].id, 3)
-        self.assertTrue(response.context['crimes'][0].id, 2)
-        self.assertTrue(response.context['crimes'][0].id, 1)
+        self.assertEquals(len(response.context['crimes']), 3)
+        self.assertEquals(response.context['crimes'][0].id, 1)
+        self.assertEquals(response.context['crimes'][1].id, 2)
+        self.assertEquals(response.context['crimes'][2].id, 3)
+
+    def test_get_most_updated(self):
+        """
+        Test accessing most updated page.
+        """
+        response = self.client.get('/most/updated/')
+        self.assertTemplateUsed(response, 'main/most_updated.html')
+
+        self.assertEquals(len(response.context['crimes']), 3)
+        self.assertEquals(response.context['crimes'][0].id, 3)
+        self.assertEquals(response.context['crimes'][1].id, 2)
+        self.assertEquals(response.context['crimes'][2].id, 1)
 
     def tearDown(self):
         pass
