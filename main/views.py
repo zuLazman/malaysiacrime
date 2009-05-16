0b0da93b0de7ec5c5a55e9d3b805aac52d24c227
Kegan Gan <kegan@kebook.local>
2009-05-16 20:37:07 +0800

    Added most updated views for crime reports.

--- main/views.py
+++ main/views.py
@@ -13,7 +13,22 @@ def index(request, template_name='main/index.html'):
     Index page.
     """
     if request.method == 'GET':
-        crimes = Crime.objects.all().order_by('-date')[:200]
+        crimes = Crime.objects.all().order_by('-date')[:100]
+    else:
+        return HttpResponseRedirect(request.path)
+
+    context = RequestContext(request, {
+        'crimes': crimes,
+        'now': datetime.now(),
+    })
+    return render_to_response(template_name, context)
+
+def most_updated(request, template_name='main/most_updated.html'):
+    """
+    Return crime reports sort by most updated.
+    """
+    if request.method == 'GET':
+        crimes = Crime.objects.all().order_by('-updated_at')[:100]
     else:
         return HttpResponseRedirect(request.path)
 
