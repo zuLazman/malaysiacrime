22b697ed939aecd0f186fb278cb610cff1497007
Kegan Gan <kegan@kebook.local>
2009-05-17 14:05:20 +0800

    Refactored retrieving model using get_model.

--- main/views.py
+++ main/views.py
@@ -5,7 +5,10 @@ from django.http import HttpResponseRedirect
 from django.shortcuts import render_to_response
 from django.template import RequestContext
 
-from crime.models import Crime
+from django.db.models import get_model
+
+
+crime_model = get_model('crime', 'crime')
 
 
 def index(request, template_name='main/index.html'):
@@ -13,7 +16,7 @@ def index(request, template_name='main/index.html'):
     Index page.
     """
     if request.method == 'GET':
-        crimes = Crime.objects.all().order_by('-date')[:100]
+        crimes = crime_model.objects.all().order_by('-date')[:100]
     else:
         return HttpResponseRedirect(request.path)
 
@@ -25,10 +28,19 @@ def index(request, template_name='main/index.html'):
 
 def recent_updated(request, template_name='main/recent_updated.html'):
     """
-    Return crime reports sort by most updated.
+    Return crime reports sort by recent updated.
     """
     if request.method == 'GET':
-        crimes = Crime.objects.all().order_by('-updated_at')[:100]
+        crimes = crime_model.objects.all().order_by('-updated_at')[:100]
+    else:
+        return HttpResponseRedirect(request.path)
+
+    context = RequestContext(request, {
+        'crimes': crimes,
+        'now': datetime.now(),
+    })
+    return render_to_response(template_name, context)
+
     else:
         return HttpResponseRedirect(request.path)
 
