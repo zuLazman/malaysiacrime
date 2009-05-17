218b604f451ddc9d599add0e94901b5c41282cf9
Kegan Gan <kegan@kebook.local>
2009-05-17 16:07:33 +0800

    Created sitemap.xml.

--- main/urls.py
+++ main/urls.py
@@ -1,10 +1,18 @@
 from django.conf.urls.defaults import *
+from django.contrib.sitemaps import GenericSitemap
+from django.db.models import get_model
 
 from views import *
 
+
+crime_model = get_model('crime', 'crime')
+crime_infodict = {'queryset': crime_model.objects.all(), 'date_field': 'updated_at'}
+sitemaps = {'blog': GenericSitemap(crime_infodict)}
 
 urlpatterns = patterns('',
     url(r'^$', index, name='main-index'),
     url(r'^recent/updated/$', recent_updated, name='main-recent-updated'),
     url(r'^recent/commented/$', recent_commented, name='main-recent-commented'),
+
+    url(r'^sitemap.xml$', 'django.contrib.sitemaps.views.sitemap', {'sitemaps': sitemaps}),
 )
