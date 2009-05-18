6dafd36a999653f041c4da20b27d8d46508270e0
Kegan Gan <kegan@kebook.local>
2009-05-18 23:53:55 +0800

    Created mat rempit GIcon.

---
+++ static/javascript/markers.js
@@ -0,0 +1,14 @@
+
+// ----- rempit icon
+var REMPIT_ICON              = new GIcon();
+REMPIT_ICON.image            = '../static/images/markers/rempit.png';
+REMPIT_ICON.shadow           = '../static/images/markers/rempit_shadow.png';
+REMPIT_ICON.mozPrintImage    = '../static/images/markers/rempit_mozPrint.gif';
+REMPIT_ICON.printImage       = '../static/images/markers/rempit_print.gif';
+REMPIT_ICON.printShadow      = '../static/images/markers/rempit_printShadow.gif';
+REMPIT_ICON.transparent      = '../static/images/markers/rempit_transparent.png';
+REMPIT_ICON.iconSize         = new GSize(39,39);
+REMPIT_ICON.shadowSize       = new GSize(59,39);
+REMPIT_ICON.iconAnchor       = new GPoint(20,39);
+REMPIT_ICON.infoWindowAnchor = new GPoint(20,0);
+REMPIT_ICON.imageMap         = [16,1,17,2,18,3,18,4,18,5,18,6,17,7,15,8,15,9,15,10,14,11,21,12,22,13,31,14,32,15,35,16,36,17,36,18,36,19,37,20,37,21,36,22,36,23,35,24,34,25,25,26,25,27,24,28,23,29,21,30,19,31,17,32,17,33,16,34,16,35,16,36,15,37,13,38,10,38,8,37,7,36,7,35,6,34,6,33,4,32,4,31,5,30,5,29,5,28,4,27,3,26,3,25,3,24,3,23,3,22,4,21,4,20,4,19,5,18,6,17,6,16,5,15,5,14,4,13,3,12,3,11,3,10,3,9,4,8,5,7,7,6,11,5,12,4,12,3,13,2,14,1];
