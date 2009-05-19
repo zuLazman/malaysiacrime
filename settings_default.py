2cfac4f9a445413c1c64e0802b82fe9b7d26fb89
Kegan Gan <kegan@kebook.local>
2009-05-19 18:03:48 +0800

    Added request context processor.

--- settings_default.py
+++ settings_default.py
@@ -75,6 +75,7 @@ TEMPLATE_CONTEXT_PROCESSORS = (
     "django.core.context_processors.auth",
     "django.core.context_processors.debug",
     "django.core.context_processors.media",
+    "django.core.context_processors.request",
 )
 
 ROOT_URLCONF = 'malaysiacrime.urls'
