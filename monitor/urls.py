acca43cb82366afff22b2a488f04625a01a0edd5
Kegan Gan <kegan@kebook.local>
2009-05-20 22:40:26 +0800

    Fixed regex in monitor urls.

--- monitor/urls.py
+++ monitor/urls.py
@@ -5,8 +5,8 @@ from views import *
 
 
 urlpatterns = patterns('',
-    url(r'subscribe/$', subscribe, name='monitor-subscribe'),
-    url(r'subscribe/done/$', subscribe_done, name='monitor-subscribe-done'),
-    url(r'subscribe/confirm/$', subscribe_confirm, name='monitor-subscribe-confirm'),
+    url(r'^subscribe/$', subscribe, name='monitor-subscribe'),
+    url(r'^subscribe/done/$', subscribe_done, name='monitor-subscribe-done'),
+    url(r'^subscribe/confirm/$', subscribe_confirm, name='monitor-subscribe-confirm'),
 
 )
