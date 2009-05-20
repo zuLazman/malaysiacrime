1405a38a3ea554137a2550bd2495bbd7cb4de0cd
Kegan Gan <kegan@kebook.local>
2009-05-20 18:41:22 +0800

    Add Moniton model that represent a monitored instance.

--- monitor/models.py
+++ monitor/models.py
@@ -1,2 +1,18 @@
 from django.db import models
 
+class Moniton(models.Model):
+    """
+    A monitoring unit. Consist of the monitored area and email.
+    """
+    email      = models.EmailField()
+    north      = models.FloatField()
+    east       = models.FloatField()
+    south      = models.FloatField()
+    west       = models.FloatField()
+    registered = models.BooleanField(default=False)
+    add_uuid   = models.CharField(max_length=32, default='', blank=True)
+    del_uuid   = models.CharField(max_length=32, default='', blank=True)
+    add_date   = models.DateTimeField(null=True)
+    del_date   = models.DateTimeField(null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
