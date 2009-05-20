9a79b41f90ce59826f3e65cbd1b2e6f62ad4eb44
Kegan Gan <kegan@kebook.local>
2009-05-20 22:39:37 +0800

    Fixed regex in crime urls.

--- crime/urls.py
+++ crime/urls.py
@@ -4,7 +4,7 @@ from views import *
 
 
 urlpatterns = patterns('',
-    url(r'show/(?P<id>\d+)/$', show, name='crime-show'),
-    url(r'create/$', create, name='crime-create'),
-    url(r'update/(?P<id>\d+)/$', update, name='crime-update'),
+    url(r'^show/(?P<id>\d+)/$', show, name='crime-show'),
+    url(r'^create/$', create, name='crime-create'),
+    url(r'^update/(?P<id>\d+)/$', update, name='crime-update'),
 )
