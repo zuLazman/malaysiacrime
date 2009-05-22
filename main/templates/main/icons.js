dfe1ae75db24f1caded348f7c4602768ebbd9027
Kegan Gan <kegan@kebook.local>
2009-05-22 14:56:28 +0800

    Updated all templates for new marker icons.

--- main/templates/main/icons.js
+++ main/templates/main/icons.js
@@ -1,14 +1,67 @@
 
 // ----- rempit icon
 var REMPIT_ICON              = new GIcon();
-REMPIT_ICON.image            = '{{ MEDIA_URL }}images/icons/rempit.png';
-REMPIT_ICON.shadow           = '{{ MEDIA_URL }}images/icons/rempit_shadow.png';
-REMPIT_ICON.mozPrintImage    = '{{ MEDIA_URL }}images/icons/rempit_mozPrint.gif';
-REMPIT_ICON.printImage       = '{{ MEDIA_URL }}images/icons/rempit_print.gif';
-REMPIT_ICON.printShadow      = '{{ MEDIA_URL }}images/icons/rempit_printShadow.gif';
-REMPIT_ICON.transparent      = '{{ MEDIA_URL }}images/icons/rempit_transparent.png';
+REMPIT_ICON.image            = '{{ MEDIA_URL }}images/icons/rempit/image.png';
+REMPIT_ICON.shadow           = '{{ MEDIA_URL }}images/icons/rempit/shadow.png';
+REMPIT_ICON.mozPrintImage    = '{{ MEDIA_URL }}images/icons/rempit/mozPrintImage.gif';
+REMPIT_ICON.printImage       = '{{ MEDIA_URL }}images/icons/rempit/printImage.gif';
+REMPIT_ICON.printShadow      = '{{ MEDIA_URL }}images/icons/rempit/printShadow.gif';
+REMPIT_ICON.transparent      = '{{ MEDIA_URL }}images/icons/rempit/transparent.png';
 REMPIT_ICON.iconSize         = new GSize(39,39);
 REMPIT_ICON.shadowSize       = new GSize(59,39);
 REMPIT_ICON.iconAnchor       = new GPoint(20,39);
 REMPIT_ICON.infoWindowAnchor = new GPoint(20,0);
 REMPIT_ICON.imageMap         = [16,1,17,2,18,3,18,4,18,5,18,6,17,7,15,8,15,9,15,10,14,11,21,12,22,13,31,14,32,15,35,16,36,17,36,18,36,19,37,20,37,21,36,22,36,23,35,24,34,25,25,26,25,27,24,28,23,29,21,30,19,31,17,32,17,33,16,34,16,35,16,36,15,37,13,38,10,38,8,37,7,36,7,35,6,34,6,33,4,32,4,31,5,30,5,29,5,28,4,27,3,26,3,25,3,24,3,23,3,22,4,21,4,20,4,19,5,18,6,17,6,16,5,15,5,14,4,13,3,12,3,11,3,10,3,9,4,8,5,7,7,6,11,5,12,4,12,3,13,2,14,1];
+
+var GUN_ICON              = new GIcon();
+GUN_ICON.image            = '{{ MEDIA_URL }}images/icons/gun/image.png';
+GUN_ICON.shadow           = '{{ MEDIA_URL }}images/icons/gun/shadow.png';
+GUN_ICON.mozPrintImage    = '{{ MEDIA_URL }}images/icons/gun/mozPrintImage.gif';
+GUN_ICON.printImage       = '{{ MEDIA_URL }}images/icons/gun/printImage.gif';
+GUN_ICON.printShadow      = '{{ MEDIA_URL }}images/icons/gun/printShadow.gif';
+GUN_ICON.transparent      = '{{ MEDIA_URL }}images/icons/gun/transparent.png';
+GUN_ICON.iconSize         = new GSize(39,39);
+GUN_ICON.shadowSize       = new GSize(59,39);
+GUN_ICON.iconAnchor       = new GPoint(20,39);
+GUN_ICON.infoWindowAnchor = new GPoint(20,0);
+GUN_ICON.imageMap         = [34,3,35,4,35,5,35,6,36,7,36,8,34,9,32,10,31,11,30,12,28,13,27,14,26,15,26,16,27,17,27,18,27,19,26,20,25,21,23,22,19,23,19,24,19,25,19,26,20,27,20,28,20,29,20,30,21,31,21,32,22,33,22,34,20,35,18,36,16,37,11,37,10,36,10,35,10,34,10,33,10,32,10,31,10,30,10,29,10,28,10,27,10,26,10,25,5,24,5,23,5,22,3,21,2,20,2,19,4,18,5,17,6,16,6,15,8,14,8,13,13,12,15,11,17,10,19,9,21,8,23,7,25,6,27,5,29,4,31,3];
+
+var PARANG_ICON              = new GIcon();
+PARANG_ICON.image            = '{{ MEDIA_URL }}images/icons/parang/image.png';
+PARANG_ICON.shadow           = '{{ MEDIA_URL }}images/icons/parang/shadow.png';
+PARANG_ICON.mozPrintImage    = '{{ MEDIA_URL }}images/icons/parang/mozPrintImage.gif';
+PARANG_ICON.printImage       = '{{ MEDIA_URL }}images/icons/parang/printImage.gif';
+PARANG_ICON.printShadow      = '{{ MEDIA_URL }}images/icons/parang/printShadow.gif';
+PARANG_ICON.transparent      = '{{ MEDIA_URL }}images/icons/parang/transparent.png';
+PARANG_ICON.iconSize         = new GSize(39,39);
+PARANG_ICON.shadowSize       = new GSize(59,39);
+PARANG_ICON.iconAnchor       = new GPoint(20,39);
+PARANG_ICON.infoWindowAnchor = new GPoint(20,0);
+PARANG_ICON.imageMap         = [34,2,35,3,36,4,37,5,36,6,34,7,35,8,35,9,32,10,30,11,29,12,29,13,28,14,27,15,26,16,26,17,25,18,24,19,23,20,23,21,22,22,21,23,20,24,19,25,18,26,17,27,16,28,15,29,14,30,13,31,12,32,11,33,9,34,8,35,6,36,4,37,2,37,2,36,2,35,3,34,3,33,3,32,3,31,4,30,4,29,4,28,5,27,6,26,7,25,9,24,10,23,11,22,12,21,13,20,14,19,15,18,16,17,17,16,18,15,19,14,21,13,22,12,23,11,24,10,25,9,26,8,27,7,28,6,27,5,27,4,32,3,33,2];
+
+var RAPE_ICON              = new GIcon();
+RAPE_ICON.image            = '{{ MEDIA_URL }}images/icons/rape/image.png';
+RAPE_ICON.shadow           = '{{ MEDIA_URL }}images/icons/rape/shadow.png';
+RAPE_ICON.mozPrintImage    = '{{ MEDIA_URL }}images/icons/rape/mozPrintImage.gif';
+RAPE_ICON.printImage       = '{{ MEDIA_URL }}images/icons/rape/printImage.gif';
+RAPE_ICON.printShadow      = '{{ MEDIA_URL }}images/icons/rape/printShadow.gif';
+RAPE_ICON.transparent      = '{{ MEDIA_URL }}images/icons/rape/transparent.png';
+RAPE_ICON.iconSize         = new GSize(39,39);
+RAPE_ICON.shadowSize       = new GSize(59,39);
+RAPE_ICON.iconAnchor       = new GPoint(20,39);
+RAPE_ICON.infoWindowAnchor = new GPoint(20,0);
+RAPE_ICON.imageMap         = [18,3,19,4,20,5,21,6,26,7,26,8,25,9,26,10,26,11,26,12,26,13,26,14,27,15,28,16,28,17,29,18,30,19,31,20,32,21,32,22,33,23,34,24,35,25,35,26,36,27,36,28,37,29,37,30,37,31,37,32,37,33,38,34,38,35,37,36,3,36,2,35,2,34,3,33,3,32,3,31,4,30,4,29,5,28,6,27,7,26,7,25,8,24,8,23,7,22,7,21,7,20,7,19,9,18,11,17,11,16,8,15,8,14,8,13,10,12,10,11,10,10,10,9,10,8,11,7,11,6,11,5,13,4,16,3];
+
+
+var SKULL_ICON              = new GIcon();
+SKULL_ICON.image            = '{{ MEDIA_URL }}images/icons/skull/image.png';
+SKULL_ICON.shadow           = '{{ MEDIA_URL }}images/icons/skull/shadow.png';
+SKULL_ICON.mozPrintImage    = '{{ MEDIA_URL }}images/icons/skull/mozPrintImage.gif';
+SKULL_ICON.printImage       = '{{ MEDIA_URL }}images/icons/skull/printImage.gif';
+SKULL_ICON.printShadow      = '{{ MEDIA_URL }}images/icons/skull/printShadow.gif';
+SKULL_ICON.transparent      = '{{ MEDIA_URL }}images/icons/skull/transparent.png';
+SKULL_ICON.iconSize         = new GSize(39,39);
+SKULL_ICON.shadowSize       = new GSize(59,39);
+SKULL_ICON.iconAnchor       = new GPoint(20,39);
+SKULL_ICON.infoWindowAnchor = new GPoint(20,0);
+SKULL_ICON.imageMap         = [22,3,24,4,26,5,27,6,27,7,28,8,28,9,29,10,29,11,29,12,29,13,29,14,29,15,28,16,28,17,33,18,33,19,33,20,34,21,35,22,35,23,35,24,31,25,29,26,27,27,28,28,30,29,34,30,35,31,35,32,34,33,34,34,33,35,33,36,6,36,6,35,6,34,5,33,4,32,5,31,5,30,9,29,11,28,13,27,10,26,8,25,5,24,4,23,5,22,5,21,6,20,6,19,7,18,11,17,10,16,10,15,10,14,10,13,10,12,10,11,10,10,10,9,11,8,11,7,12,6,13,5,15,4,17,3];
